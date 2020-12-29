a685ccb6df7b520efee5ed9196ed25dfad17775d
Heluan <heluan.caetano@gmail.com>
2020-12-29 01:54:37 -0300

    Astronaltas a bordo

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -12,42 +12,43 @@ import {
 } from 'react-native';
 import api from '../../Services/api';
 
-interface RegionX {
-  latitude: string | number ;
-  longitude: string | number;
-  latitudeDelta: number;
-  longitudeDelta: number;
-};
-
 const Home: React.FC = () => {
-  const [localization, setLocalization] = useState<RegionX[]>({} as RegionX[]);
+  const [localization, setLocalization] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0,
+    longitudeDelta: 0,
+  });
+  const [astros, setAstros] = useState({
+    message: "",
+    number: 0,
+    people: {
+      craft: "", 
+      name: "",
+    },
+  });
   
   useEffect(()=>{
     const interval = setInterval(() => {
       getISS();
     }, 1000);
-    console.log(localization)
+    // console.log(localization)
     return () => clearInterval(interval);
   });
+
+  useEffect(()=>{
+    getAstros();
+  },[])
 
   async function getISS() {
     const geolocation = await api.get('/iss-now.json');
     setLocalization(geolocation.data.iss_position);
   }
 
-  function getInitialState() {
-    return {
-      region: {
-        latitude: localization.latitude,
-        longitude: localization.longitude,
-        latitudeDelta: 10,
-        longitudeDelta: 10,
-      },
-    };
-  }
-  
-  function onRegionChange(region) {
-    setLocalization(region);
+  async function getAstros() {
+    const astrosX = await api.get('/astros.json');
+    setAstros(astrosX.data);
+    console.log(astros)
   }
 
   return (
@@ -55,9 +56,9 @@ const Home: React.FC = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <View>
-        <Text style={{textAlign: 'center'}}>
-         Space Station Pass
-        </Text>
+          <Text style={{textAlign: 'center'}}>
+            Space Station Pass
+          </Text>
         </View>
         <View>
         <MapView
@@ -73,22 +74,41 @@ const Home: React.FC = () => {
           region={{
           latitude: localization.latitude,
           longitude: localization.longitude,
-          latitudeDelta: 10,
-          longitudeDelta: 10,
+          latitudeDelta: 100,
+          longitudeDelta: 100,
           }}
-          onRegionChange={onRegionChange}
+          toolbarEnabled={true}
+          zoomControlEnabled={true}
         >
           <Marker 
           coordinate={{
              latitude : localization.latitude, 
              longitude : localization.longitude,
             }}
+            title={"Iss"}
+            description={"description"}
+            pinColor = {"purple"}
           >
             <Text style={{color: '#fff'}}>ISS</Text>
           </Marker>
         </MapView>
         </View>
-        
+        <View style={{marginTop: 350}}>
+            <Text>Aeronave :{astros.people[0].craft}</Text>
+            <Text>Astronalta :{astros.people[0].name}</Text>
+            <Text>Aeronave :{astros.people[1].craft}</Text>
+            <Text>Astronalta :{astros.people[1].name}</Text>
+            <Text>Aeronave :{astros.people[2].craft}</Text>
+            <Text>Astronalta :{astros.people[2].name}</Text>
+            <Text>Aeronave :{astros.people[3].craft}</Text>
+            <Text>Astronalta :{astros.people[3].name}</Text>
+            <Text>Aeronave :{astros.people[4].craft}</Text>
+            <Text>Astronalta :{astros.people[4].name}</Text>
+            <Text>Aeronave :{astros.people[5].craft}</Text>
+            <Text>Astronalta :{astros.people[5].name}</Text>
+            <Text>Aeronave :{astros.people[6].craft}</Text>
+            <Text>Astronalta :{astros.people[6].name}</Text>
+        </View>
       </SafeAreaView>
     </>
   );
